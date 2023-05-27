b5ff48064025a221fbe1499f5af8f79712cf98a9
Rebecca Choo <rnwldms@gmail.com>
2023-05-27 00:05:35 -0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sample';
+  ngOnInit(): void {
+    particlesJS.load('particles', 'assets/particles.json', () => {
+      console.log('particles.js config loaded');
+    });
+  }
 }
